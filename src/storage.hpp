e8dca37ec34fac6a650789b951d10b6b37753fd4
Parenbz <parenbz@yandex.ru>
2025-05-22 09:20:43 +0300

    Added store and load

--- src/storage.hpp
+++ src/storage.hpp
@@ -2,6 +2,8 @@
 
 #include <cstddef>
 #include <stdexcept>
+#include <unordered_map>
+#include <shared_mutex>
 
 template<typename KeyT, typename ValueT>
 class Storage {
@@ -19,7 +21,28 @@ public:
             }
         }
 
+    void store(const KeyT& key, const ValueT& value) {
+        std::unique_lock lock(mutex_);
+
+        if (storage_.size() >= capacity_ && storage_.find(key) == storage_.end()) {
+            throw std::runtime_error("Storage capacity exceeded");
+        }
+
+        storage_[key] = value;
+    }
+
+    ValueT load(const KeyT& key) {
+        std::shared_lock lock(mutex_);
+        auto it = storage_.find(key);
+        if (it == storage_.end()) {
+            throw std::out_of_range("Key not found");
+        }
+        return it->second;
+    }
+
 private:
     std::size_t capacity_;
     std::size_t cache_size_;
+    std::unordered_map<KeyT, ValueT> storage_;
+    mutable std::shared_mutex mutex_;
 };
