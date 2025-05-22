903ba2c46c3fcd6d3e71742834392034bf600188
Parenbz <parenbz@yandex.ru>
2025-05-22 09:01:57 +0300

    Tests throw on capacity/cache size errors

--- tests/unit.cpp
+++ tests/unit.cpp
@@ -5,3 +5,15 @@
 TEST(StorageTest, CanCreateAndDestroy) {
     Storage<int, std::string> storage(100, 10);
 }
+
+TEST(StorageTest, ThrowsOnZeroCapacity) {
+    EXPECT_THROW((Storage<int, int>(10, 0)), std::invalid_argument);
+}
+
+TEST(StorageTest, ThrowsOnZeroCacheSize) {
+    EXPECT_THROW((Storage<int, int>(10, 0)), std::invalid_argument);
+}
+
+TEST(StorageTest, ThrowsWhenCacheLargerThanCapacity) {
+    EXPECT_THROW((Storage<int, int>(10, 20)), std::invalid_argument);
+}
