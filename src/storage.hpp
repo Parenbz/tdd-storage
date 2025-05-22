7d71a031dc253b39f1c51b24841c86e0720ec352
Parenbz <parenbz@yandex.ru>
2025-05-22 08:51:52 +0300

    Added storage class

--- src/storage.hpp
+++ src/storage.hpp
@@ -1,5 +1,15 @@
 #pragma once
 
-int tmp() {
-    return 0;
-}
+#include <cstddef>
+#include <stdexcept>
+
+template<typename KeyT, typename ValueT>
+class Storage {
+public:
+    Storage(std::size_t capacity, std::size_t cache_size)
+        : capacity_(capacity), cache_size_(cache_size) {}
+
+private:
+    std::size_t capacity_;
+    std::size_t cache_size_;
+};
