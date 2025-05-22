9bc3496a78195ee66e2470d7cfd47c13f2888caf
Parenbz <parenbz@yandex.ru>
2025-05-22 09:05:34 +0300

    Fixed size errors

--- src/storage.hpp
+++ src/storage.hpp
@@ -7,7 +7,17 @@ template<typename KeyT, typename ValueT>
 class Storage {
 public:
     Storage(std::size_t capacity, std::size_t cache_size)
-        : capacity_(capacity), cache_size_(cache_size) {}
+        : capacity_(capacity), cache_size_(cache_size) {
+            if (capacity_ == 0) {
+                throw std::invalid_argument("Capacity = 0");
+            }
+            if (cache_size_ == 0) {
+                throw std::invalid_argument("Cache = 0");
+            }
+            if (cache_size_ > capacity_) {
+                throw std::invalid_argument("Cache size exceeds capacity");
+            }
+        }
 
 private:
     std::size_t capacity_;
